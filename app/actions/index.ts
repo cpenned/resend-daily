70132da55a57d15e27441edd51806b1c6cbdb97b
Chris Pennington <cpenn368@gmail.com>
2025-01-16 10:26:39 -0700

    update action remove unused hook

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,44 +1,53 @@
-"use server"
+"use server";
 // useCollection.ts
 import { db } from "@/lib/firebase/config";
-import { collection, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import {
+	collection,
+	doc,
+	getDocs,
+	query,
+	updateDoc,
+	where,
+} from "firebase/firestore";
 
-export const movePeople = async (collectionName: string, action: "resetAll" | "markDone" | "markActive", id: string) => {
+export const movePeople = async (
+	collectionName: string,
+	action: "resetAll" | "markDone" | "markActive",
+	id: string,
+) => {
+	const collectionRef = collection(db, collectionName);
 
-    const collectionRef = collection(db, collectionName);
+	if (action === "markActive") {
+		const q = query(collectionRef, where("status", "==", "active"));
+		const querySnapshot = await getDocs(q);
 
-    if(action === "markActive"){
-      const q = query(collectionRef, where("status", "==", "active"));
-      const querySnapshot = await getDocs(q);
+		for (const doc of querySnapshot.docs) {
+			await updateDoc(doc.ref, {
+				status: "done",
+			});
+		}
 
-      querySnapshot.forEach(async (doc) => {
-        await updateDoc(doc.ref, {
-          status: "done"
-        })
-      });
+		await updateDoc(doc(db, collectionName, id), {
+			status: "active",
+		});
+	}
 
-      await updateDoc(doc(db, collectionName, id), {
-          status: "active"
-      });
-    }
+	if (action === "resetAll") {
+		const querySnapshot = await getDocs(collectionRef);
+		for (const doc of querySnapshot.docs) {
+			await updateDoc(doc.ref, {
+				status: "future",
+			});
+		}
+	}
 
-    if(action === "resetAll"){
-      const querySnapshot = await getDocs(collectionRef);
-      return querySnapshot.forEach(async (doc) => {
-        await updateDoc(doc.ref, {
-          status: "future"
-        })
-      });
-    }
-
-    if(action === "markDone"){
-      const q = query(collectionRef, where("status", "==", "active"));
-      const querySnapshot = await getDocs(q);
-      return querySnapshot.forEach(async (doc) => {
-        await updateDoc(doc.ref, {
-          status: "done"
-        })
-      });
-    }
+	if (action === "markDone") {
+		const q = query(collectionRef, where("status", "==", "active"));
+		const querySnapshot = await getDocs(q);
+		for (const doc of querySnapshot.docs) {
+			await updateDoc(doc.ref, {
+				status: "done",
+			});
+		}
+	}
 };
-
